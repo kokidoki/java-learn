6cd3a3bf40ebc5275cadb6f006b1fa2d2a473924
Kenny Deng <kennydeng352@gmail.com>
2020-11-14 20:04:33 -0500

    Learn Lesson 4

---
+++ basics/lesson4_functions.java
@@ -0,0 +1,48 @@
+public class lesson4_functions {
+	public static void main(String[] args) {
+		//JAVA HAS NO FUNCTIONS — ONLY METHODS(METHODS ARE ALWAYS INSIDE CLASS)
+
+
+		// printName();
+		// printFancyName("kennì");
+		// printFancyName("¬î¬¥");
+		// printFancyFancyName("kenn", "den", 3);
+		// printFancyFancyName("lil", "den", 78);
+		// walk(15);
+		// vacuum();
+		// vacuum("k");
+		// vacuum("k", "e", "m", "n", "y");
+		System.out.println(sum(1, 1));
+		System.out.println(sum(1.0, 2.0));
+
+	}
+	public static void printName() {
+		System.out.println("kénñ¥");
+	}
+	public static void printFancyName(String name) {
+		System.out.println(name);
+	}
+	public static void printFancyFancyName(String firstName, String lastName, Integer fav_num) {
+		System.out.println(firstName + " " + lastName + " likes " + fav_num);
+	}
+	public static void walk(Integer step) {
+		if (step > 0) {
+			System.out.println("Step number " + step);
+			walk(--step);
+		}
+	}
+	public static void vacuum(String ...bag) {
+		for (String dust : bag) {
+			System.out.print(dust);
+		}
+	}
+
+	// METHOD OVERLOADING — loading same name with different arg type
+	// Method Signature = Uniqueness(Return type, name, argument name)(Ex: Integer, Sum, Integer)
+	public static Integer sum(Integer a, Integer b) {
+		return a + b;
+	}
+	public static Double sum(Double a, Double b) {
+		return a + b;
+	}
+}
