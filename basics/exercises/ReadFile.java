3633897a546bdcd51c1ab85896cb41875ca371e1
Kenny Deng <kennydeng352@gmail.com>
2020-11-22 13:57:54 -0500

    Complete Read File Exercise

---
+++ basics/exercises/ReadFile.java
@@ -0,0 +1,40 @@
+import java.io.*;
+public class ReadFile {
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+		// BufferedReader bReader = null;
+		// try {
+		// 	bReader = new BufferedReader(new FileReader("/Users/kenny/java-learn/basics/Lesson5_CAFH.java"));
+		// 	String output = bReader.readLine();
+		// 	while (output != null) {
+		// 		System.out.println(output);
+		// 		output = bReader.readLine();
+		// 	}
+		// } finally {
+		// 	if (bReader != null) {
+		// 		bReader.close();
+		// 	}
+		// }
+
+
+
+		// try (BufferedReader bReader = new BufferedReader(new FileReader(args[0]))) {
+		// 	String output = bReader.readLine();
+		// 	while (output != null) {
+		// 		System.out.println(output);
+		// 		output = bReader.readLine();
+		// 	}
+		// }
+
+
+
+		try (BufferedReader bReader = new BufferedReader(new FileReader(args[0]))) {
+			String output = null;
+			do {
+				output = bReader.readLine();
+				if(output != null) {
+					System.out.println(output);
+				}
+			} while (output != null);
+		}
+	}
+}
