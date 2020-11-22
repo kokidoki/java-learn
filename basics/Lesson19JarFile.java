b3d73101e2edad7b86f2610197edc58746f85372
Kenny Deng <kennydeng352@gmail.com>
2020-11-22 17:27:49 -0500

    Learn Lesson 19

---
+++ basics/Lesson19JarFile.java
@@ -0,0 +1,8 @@
+// Creating a jar from all .class files in current dir: find lesson19 -name '*.class' | xargs jar -cvf Lesson19.jar
+
+import lesson19.*;
+public class Lesson19JarFile {
+	public static void main(String[] args) {
+		Main.main(args);
+	}
+}
---
+++ basics/lesson19/Main.java
@@ -0,0 +1,9 @@
+package lesson19;
+public class Main {
+	public static void main(String[] args) {
+		Apple gala = new Apple();
+		Banana banana = new Banana();
+		System.out.println(gala);
+		System.out.println(banana);
+	}
+}
