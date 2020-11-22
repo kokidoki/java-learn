64762989b9c2acfd3fce440b0bf2f4e2e658a65b
Kenny Deng <kennydeng352@gmail.com>
2020-11-21 21:55:46 -0500

    Add finally block

--- basics/Lesson15ExceptionHandling.java
+++ basics/Lesson15ExceptionHandling.java
@@ -3,21 +3,23 @@
 
 public class Lesson15ExceptionHandling {
 	public static void main(String[] args) {
-		// try {
-		// 	int a = Integer.valueOf(args[0]);
-		// 	int b = Integer.valueOf(args[1]);
-		// 	System.out.println(a / b);
-		// } catch(ArithmeticException | NumberFormatException arithmeticErr) {
-		// 	System.out.println("WRONG INPUT");
-		// } catch(Exception exceptionErr) { // Not doing anything with exception catching is called "Swallowing," don't do this!
-		// 	// System.out.println("Something Broke!");
-		// 	// System.out.println(err.getStackTrace());
-		// 	// err.printStackTrace();
-		// 	// System.out.println(exceptionErr.getClass().getName());
-		// }
+		try {
+			int a = Integer.valueOf(args[0]);
+			int b = Integer.valueOf(args[1]);
+			System.out.println(a / b);
+		} catch(ArithmeticException | NumberFormatException arithmeticErr) {
+			System.out.println("WRONG INPUT");
+		} catch(Exception exceptionErr) { // Not doing anything with exception catching is called "Swallowing," don't do this!
+			// System.out.println("Something Broke!");
+			// System.out.println(err.getStackTrace());
+			// err.printStackTrace();
+			// System.out.println(exceptionErr.getClass().getName());
+		} finally {
+			System.out.println("Thank you for using our services");
+		}
 
 
-		testDivision();
+		// testDivision();
 	}
 
 	public static void testDivision() {
@@ -41,13 +43,13 @@ public class Lesson15ExceptionHandling {
 		return int1 / int2;
 	}
 	public static int divide2(int int1, int int2) throws BadBadException {
+		if(int2 == 0) {
 			throw new BadBadException("Invalid argument: " + int2);
 		}
 		return int1 / int2;
 	}
 
 	public static class BadNumberException extends RuntimeException{  // UNCHECKED EXCEPTION(EXTENDS EXCEPTION)
-		if (int2 == 0) {
 		public BadNumberException(String message) {
 			super(message);
 		}
