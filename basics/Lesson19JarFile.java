ee14510712697f20cb9b135b5cb94524acab1bbb
Kenny Deng <kennydeng352@gmail.com>
2020-11-22 17:39:37 -0500

    Notes on jar commands

--- basics/Lesson19JarFile.java
+++ basics/Lesson19JarFile.java
@@ -1,5 +1,11 @@
-// Creating a jar from all .class files in current dir: find lesson19 -name '*.class' | xargs jar -cvf Lesson19.jar
+/*
+Creating a jar from all .class files in current dir: find <path> -name '*.class' | xargs jar -cvf <jar name>
+Running main file in jar: java -cp <jar name> <main class to run>
+Listing files in jar: jar -tvf <jar name>
 
+To clean directory, pipe all .class files into xargs and rm -v
+ALWAYS SAVE AND PUSH BEFORE DELETING
+*/
 import lesson19.*;
 public class Lesson19JarFile {
 	public static void main(String[] args) {
